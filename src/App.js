4cb775fa82c320eb2735dea97a85462505430fc7
chiuchiuleuleu <hghjb@nkjsna>
2024-05-07 13:50:18 +0700

    sua duong dan Home

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ import Room from "./pages/room/Room";
 import Device from "./pages/device/Device";
 import AddDevice from "./pages/device/AddDevice";
 import EditDevice from "./pages/device/EditDevice";
+import ClientHome from "./pages/home/client/ClientHome";
+import Profile from "./pages/home/client/Profile";
+import ClientInvoice from "./pages/home/client/ClientInvoice";
+import Request from "./pages/home/client/Request";
 
 function App() {
   return (
@@ -25,24 +29,29 @@ function App() {
           </div>
           <div className="row">
               <div className="col-12">
-                  {/*<Routes>*/}
-                  {/*    <Route to={`home`} element={<Home/>}></Route>*/}
-                  {/*    <Route to={`admin`} element={<Admin/>}>*/}
-                  {/*        <Route path={``} element={<ListUser/>}/>*/}
-                  {/*        <Route path={`room`} element={<Room/>}>*/}
-                  {/*            <Route path={``} element={<ListRoom/>}/>*/}
-                  {/*            <Route path={`add`} element={<AddRoom/>}/>*/}
-                  {/*            <Route path={`edit`} element={<EditRoom/>}/>*/}
-                  {/*        </Route>*/}
-                  {/*        <Route path={`invoice`} element={<ListInvoice/>}/>*/}
-                  {/*        <Route path={`device`} element={<Device/>}>*/}
-                  {/*            <Route path={``} element={<ListDevice/>}/>*/}
-                  {/*            <Route path={`add`} element={<AddDevice/>}/>*/}
-                  {/*            <Route path={`edit`} element={<EditDevice/>}/>*/}
-                  {/*        </Route>*/}
-                  {/*    </Route>*/}
-                  {/*</Routes>*/}
-                  <Home></Home>
+                  <Routes>
+                      <Route to={``} element={<Home/>}>
+                              <Route path={``} element={<ClientHome/>}></Route>
+                              <Route path={`profile`} element={<Profile/>}></Route>
+                              <Route path={`invoice`} element={<ClientInvoice/>}></Route>
+                              <Route path={`request`} element={<Request/>}></Route>
+                      </Route>
+                      <Route to={`admin`} element={<Admin/>}>
+                          <Route path={``} element={<ListUser/>}/>
+                          <Route path={`room`} element={<Room/>}>
+                              <Route path={``} element={<ListRoom/>}/>
+                              <Route path={`add`} element={<AddRoom/>}/>
+                              <Route path={`edit`} element={<EditRoom/>}/>
+                          </Route>
+                          <Route path={`invoice`} element={<ListInvoice/>}/>
+                          <Route path={`device`} element={<Device/>}>
+                              <Route path={``} element={<ListDevice/>}/>
+                              <Route path={`add`} element={<AddDevice/>}/>
+                              <Route path={`edit`} element={<EditDevice/>}/>
+                          </Route>
+                      </Route>
+                  </Routes>
+
               </div>
           </div>
       </div>
