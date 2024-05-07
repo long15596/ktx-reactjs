efc7954ac3f8d123437bccdedcd11080b8bcf0b3
chiuchiuleuleu <hghjb@nkjsna>
2024-05-07 14:31:34 +0700

    hiển thị nút xem thêm

--- src/pages/home/client/ClientHome.js
+++ src/pages/home/client/ClientHome.js
@@ -1,16 +1,19 @@
-import {useDispatch, useSelector} from "react-redux";
-import {useEffect} from "react";
-import {getRooms} from "../../../services/RoomService";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRooms } from "../../../services/RoomService";
 
 export default function ClientHome() {
-    const dispatch = useDispatch()
-    const rooms = useSelector(state => {
-        console.log(state.rooms.rooms)
-        return state.rooms.rooms
-    })
+    const dispatch = useDispatch();
+    const [visibleRooms, setVisibleRooms] = useState(6); // Số lượng phòng hiển thị ban đầu
+    const rooms = useSelector(state => state.rooms.rooms);
+
     useEffect(() => {
-        dispatch(getRooms())
+        dispatch(getRooms());
     }, []);
+
+    const handleShowMore = () => {
+        setVisibleRooms(prev => prev + 6); // Khi nhấn nút Xem thêm, hiển thị thêm 6 phòng
+    };
 
     return (
         <>
@@ -21,32 +24,43 @@ export default function ClientHome() {
 
                 <div className="container">
                     <div className="row">
-                        {rooms.map(room => (
+                        {rooms.slice(0, visibleRooms).map(room => (
                             <div className="col-4" key={room.id}>
-                                <div className="card">
-                                    <img
-                                        src={room.img}
-                                        className="card-img-top" alt="..."
-                                    />
-                                    <div className="card-body">
-                                        <h5 className="card-title">{room.name}</h5>
-                                        <p className="card-text">Số người: {room.currentPresent}/{room.maxCurrent}</p>
-                                        <p className="card-text">Kiểu: {room.type}</p>
-                                        <p className="card-text">Mô tả: {room.description}</p>
-                                        <p className="card-text">Giá: {room.price}</p>
+                                <div className="col-4" key={room.id}>
+                                    <div className="col-4" key={room.id}>
+                                        <div className="card">
+                                            <img
+                                                src={room.img}
+                                                className="card-img-top" alt="..."
+                                            />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{room.name}</h5>
+                                                <p className="card-text">Số người: {room.currentPresent}/{room.maxCurrent}</p>
+                                                <p className="card-text">Kiểu: {room.type}</p>
+                                                <p className="card-text">Mô tả: {room.description}</p>
+                                                <p className="card-text">Giá: {room.price}</p>
+                                                <div className={'d-flex justify-content-end'}>
+                                                    <a href="#" className="btn btn-outline-primary">Thuê ngay!!!</a>
+                                                </div>
 
-                                        <div className={'d-flex justify-content-end'}>
-                                            <a href="#" className="btn btn-outline-primary">Thuê ngay!!!</a>
+                                            </div>
                                         </div>
-
                                     </div>
                                 </div>
                             </div>
                         ))}
                     </div>
+                    {visibleRooms < rooms.length && (
+                        <div className="row">
+                            <div className="col-12 d-flex justify-content-center">
+                                <button className="mt-2 btn btn-outline-primary" onClick={handleShowMore}>
+                                    Xem thêm
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
-
             </div>
         </>
-    )
+    );
 }
