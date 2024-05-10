60f4ecd7a1981d9cf5b9f32e5498c6dc982c4a51
NgocLong155 <ngoclong15596@gmail.com>
2024-05-10 10:25:07 +0700

    pull dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import roomReducer from "./room/RoomsSlice";
 import deviceReducer from "./device/devicesSlice";
 import userReducer from "./user/UserSlice";
 import invoiceReducer from "./invoice/InvoicesSlice";
-import roomDeviceReducer from "./roomDevice/roomDeviceSlice";
+import roomDevicesReducer from "./roomDevice/roomDeviceSlice";
 
 export let store = configureStore({
     reducer:{
@@ -11,6 +11,6 @@ export let store = configureStore({
         rooms: roomReducer,
         devices: deviceReducer,
         user:userReducer,
-        roomsDevice: roomDeviceReducer
+        roomDevices: roomDevicesReducer
     },
 })
