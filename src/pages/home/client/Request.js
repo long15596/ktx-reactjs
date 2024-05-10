2900341386e72d6d8b0433ca6695476fbb95e481
chiuchiuleuleu <hghjb@nkjsna>
2024-05-10 11:44:18 +0700

    pull dev

--- src/pages/home/client/Request.js
+++ src/pages/home/client/Request.js
@@ -5,6 +5,7 @@ import {addProfile} from "../../../services/usersServices/UserService";
 import {Field, Form, Formik} from "formik";
 import FireUpload from "../../../components/FireUpload";
 import Swal from "sweetalert2";
+import * as Yup from "yup";
 import {getRooms} from "../../../services/roomsServices/RoomService";
 
 export default function Request() {
@@ -66,6 +67,15 @@ export default function Request() {
         }
     }
 
+
+    const validationSchema = Yup.object().shape({
+        username: Yup.string()
+            .matches(/^[0-9]{9}$/, 'Mã sinh viên phải là 9 chữ số')
+            .required('Không được để trống mã sinh viên')
+    });
+
+
+
     return (
         <>
             <div className="container">
@@ -74,6 +84,7 @@ export default function Request() {
                     onSubmit={(values) => {
                         handleAdd(values)
                     }}
+                    validationSchema={validationSchema}
                     initialValues={{
                         username: "",
                         password: "",
@@ -85,84 +96,87 @@ export default function Request() {
                         gender: gender,
                         img: url
                     }}>
-                    <Form className={"mt-3"}>
-                        <div className="row">
-                            <div className="col-md-9">
-                                <div className="form-group">
-                                    <label htmlFor="inputMSV">Mã Sinh Viên</label>
-                                    <Field type="text" name={"username"} className="form-control" id="inputMSV"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="inputpassword">Mật Khẩu</label>
-                                    <Field type="password" name={"password"} className="form-control"
-                                           id="inputpassword"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="inputAddress">Địa Chỉ</label>
-                                    <Field type="text" name={"address"} className="form-control" id="inputAddress"
-                                           placeholder="Hà Nội..."/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="inputBirthDay">Ngày Sinh</label>
-                                    <Field type="date" name={"dateOfBirth"} className="form-control"
-                                           id="inputBirthDay"/>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="inputIdentityID">Số CCCD</label>
-                                    <Field type="text" name={"identificationCard"} className="form-control"
-                                           id="inputIdentityID"
-                                           placeholder="00123456789..."/>
-                                </div>
-                                <div className="form-row">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="inputPhone">Số điện thoại</label>
-                                        <Field type="text" name={"phone"} className="form-control" id="inputPhone"/>
+                    {({ errors, touched }) => ( // Destructure errors and touched from Formik props
+                        <Form className={"mt-3"}>
+                            <div className="row">
+                                <div className="col-md-9">
+                                    {/* InputMSV Field */}
+                                    <div className="form-group">
+                                        <label htmlFor="inputMSV">Mã Sinh Viên</label>
+                                        <Field type="text" name={"username"} className="form-control" id="inputMSV"/>
+                                        {touched.username && errors.username && (
+                                            <div><p className={'text-danger'}>{errors.username}</p></div>
+                                        )}
                                     </div>
-                                    <div className="form-group col-md-4">
-                                        <label htmlFor="inputGender" > Giới tính</label>
-                                        <select onChange={(event)=>{
-                                           setGender(event.target.value)
-                                        }} name={"gender"} id="inputGender" className="form-control">
-                                            <option >Lựa chọn</option>
-                                            <option value={"Nam"}>Nam</option>
-                                            <option value={"Nữ"}>Nữ</option>
-                                            <option value="Khác">Khác...</option>
+                                    <div className="form-group">
+                                        <label htmlFor="inputpassword">Mật Khẩu</label>
+                                        <Field type="password" name={"password"} className="form-control"
+                                               id="inputpassword"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="inputAddress">Địa Chỉ</label>
+                                        <Field type="text" name={"address"} className="form-control" id="inputAddress"
+                                               placeholder="Hà Nội..."/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="inputBirthDay">Ngày Sinh</label>
+                                        <Field type="date" name={"dateOfBirth"} className="form-control"
+                                               id="inputBirthDay"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="inputIdentityID">Số CCCD</label>
+                                        <Field type="text" name={"identificationCard"} className="form-control"
+                                               id="inputIdentityID"
+                                               placeholder="00123456789..."/>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="form-group col-md-6">
+                                            <label htmlFor="inputPhone">Số điện thoại</label>
+                                            <Field type="text" name={"phone"} className="form-control" id="inputPhone"/>
+                                        </div>
+                                        <div className="form-group col-md-4">
+                                            <label htmlFor="inputGender" > Giới tính</label>
+                                            <select onChange={(event)=>{
+                                                setGender(event.target.value)
+                                            }} name={"gender"} id="inputGender" className="form-control">
+                                                <option >Lựa chọn</option>
+                                                <option value={"Nam"}>Nam</option>
+                                                <option value={"Nữ"}>Nữ</option>
+                                                <option value="Khác">Khác...</option>
+                                            </select>
+                                        </div>
+                                        <div className="form-group col-md-2">
+                                            <label htmlFor="inputClazz">Lớp?</label>
+                                            <Field type="text" name={"clazz"} className="form-control" id="inputClazz"/>
+                                        </div>
+
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="inputRoom"> Danh sách các phòng phù hợp</label>
+                                        <select name={"room"} id="inputRoom" className="form-control">
+                                            <option selected>Lựa chọn</option>
+                                            {rooms.map(room=>{
+                                                if(room.type === gender && (room.maxCurrent - room.currentPresent) >0){
+                                                    console.log(room)
+                                                    return (<option value={room.id}>{room.name}</option>)
+                                                }
+                                            })}
                                         </select>
                                     </div>
-                                    <div className="form-group col-md-2">
-                                        <label htmlFor="inputClazz">Lớp?</label>
-                                        <Field type="text" name={"clazz"} className="form-control" id="inputClazz"/>
-                                    </div>
-
                                 </div>
-                                <div className="form-group">
-                                    <label htmlFor="inputRoom"> Danh sách các phòng phù hợp</label>
-                                    <select name={"room"} id="inputRoom" className="form-control">
-                                        <option selected>Lựa chọn</option>
-                                        {rooms.map(room=>{
-                                            if(room.type === gender && (room.maxCurrent - room.currentPresent) >0){
-                                                console.log(room)
-                                                return (<option value={room.id}>{room.name}</option>)
-                                            }
-                                        })}
-                                    </select>
+                                <div className="col-3 avatar-edit text-center">
+                                    <img
+                                        src={url ? url : "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg"}
+                                        className={"avatar img-thumbnail"} alt="avatar"/>
+                                    <FireUpload onUpload={(onUpload) => {
+                                        setUrl(onUpload)
+                                    }}></FireUpload>
                                 </div>
                             </div>
-                            <div className="col-3 avatar-edit text-center">
-                                <img
-                                    src={url ? url : "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg"}
-                                    className={"avatar img-thumbnail"} alt="avatar"/>
-                                <FireUpload onUpload={(onUpload) => {
-                                    setUrl(onUpload)
-                                }}></FireUpload>
-                            </div>
-
-
-                        </div>
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Đăng Ký</button>
-
-
-                    </Form>
+                            {/* Submit button */}
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Đăng Ký</button>
+                        </Form>
+                    )}
                 </Formik>
 
             </div>
