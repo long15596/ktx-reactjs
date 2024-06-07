fb64ee2dc61bad95e8c4d6b60053bc7f8598e7b9
nguyendinhdat05 <dat09092005@gmail.com>
2024-06-07 14:26:04 +0700

    pull long

--- src/pages/user/ListUser.js
+++ src/pages/user/ListUser.js
@@ -3,20 +3,22 @@ import {useDispatch, useSelector} from "react-redux";
 import {changeEnabled, getAllUserByAdmin, searchUser} from "../../services/usersServices/UserService";
 import './listUser.css';
 import {Link} from "react-router-dom";
-import {searchRoom} from "../../services/roomsServices/RoomService";
 
 export default function ListUser() {
-    const dispatch = useDispatch();
-    const listUsersSearch = useSelector(state => state.user.listUserSearch);
-    const users = useSelector(state => {
-        if (listUsersSearch && listUsersSearch.length > 0 ){
-            return listUsersSearch
-        }
+    let dispatch = useDispatch();
+    let [value, setValue] = useState('')
+    let listUsersSearch = useSelector(state => state.user.listUserSearch);
+    let users = useSelector(state => {
+            if (listUsersSearch && listUsersSearch.length > 0) {
+                return listUsersSearch
+            }
             return state.user.user
         }
     );
-    const [currentPage, setCurrentPage] = useState(1);
-    const [usersPerPage] = useState(6);
+
+    console.log(value)
+    let [currentPage, setCurrentPage] = useState(1);
+    let [usersPerPage] = useState(6);
     useEffect(() => {
         dispatch(getAllUserByAdmin());
         dispatch(searchUser())
@@ -31,11 +33,8 @@ export default function ListUser() {
     const indexOfLastUser = currentPage * usersPerPage;
     const indexOfFirstUser = indexOfLastUser - usersPerPage;
     const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
-
     const totalPageCount = Math.ceil(users.length / usersPerPage);
-
     const paginate = pageNumber => setCurrentPage(pageNumber);
-
     return (
         <>
             <div className="container-fluid d-flex">
@@ -47,6 +46,7 @@ export default function ListUser() {
                         placeholder="tìm kiếm sinh viên"
                         aria-label="Search"
                         onChange={(e) => {
+                            setValue(e.target.value)
                             dispatch(searchUser(e.target.value))
                         }}
                     />
@@ -70,36 +70,35 @@ export default function ListUser() {
                     </tr>
                     </thead>
                     <tbody>
-                    {currentUsers.map((user, index) => (
-                        <tr key={index}>
-                            <th scope="row">{user.id}</th>
-                            <td><img src={user.img} id={`img-table`} alt={`img-room`}/></td>
-                            <td>{user.username}</td>
-                            <td>{user.clazz}</td>
-                            <td>{user.dateOfBirth}</td>
-                            <td>{user.phone}</td>
-                            <td>{user.address}</td>
-                            <td>{user.identificationCard}</td>
-                            <td>{user.enabled ? "Hoạt động" : "Đã khóa"}</td>
-                            <td>
-                                <button
-                                    className={user.enabled ? `btn btn-outline-danger` : `btn btn-outline-success`}
-                                    type="submit" onClick={() => {
-                                    handleChangeEnabled(user.id)
-                                }}>{user.enabled ? "Khóa" : "Mở Khóa"}</button>
-                            </td>
-                            <td>
-                                <Link to={`user/${user.id}`} className={`btn btn-outline-primary`}>Thông
-                                    Tin</Link>
-                            </td>
-                        </tr>
-                    ))}
+                    {
+                        currentUsers.map((user, index) => (
+                            <tr key={index}>
+                                <th scope="row">{user.id}</th>
+                                <td><img src={user.img} id={`img-table`} alt={`img-room`}/></td>
+                                <td>{user.username}</td>
+                                <td>{user.clazz}</td>
+                                <td>{user.dateOfBirth}</td>
+                                <td>{user.phone}</td>
+                                <td>{user.address}</td>
+                                <td>{user.identificationCard}</td>
+                                <td>{user.enabled ? "Hoạt động" : "Đã khóa"}</td>
+                                <td>
+                                    <button
+                                        className={user.enabled ? `btn btn-outline-danger` : `btn btn-outline-success`}
+                                        type="submit" onClick={() => {
+                                        handleChangeEnabled(user.id)
+                                    }}>{user.enabled ? "Khóa" : "Mở Khóa"}</button>
+                                </td>
+                                <td>
+                                    <Link to={`user/${user.id}`} className={`btn btn-outline-primary`}>Thông
+                                        Tin</Link>
+                                </td>
+                            </tr>
+                        ))
+
+                    }
                     </tbody>
                 </table>
-                {/*    </div>*/}
-                {/*</div>*/}
-                {/*<div className="row">*/}
-                {/*    <div className="col-12">*/}
                 <div className="pagination">
                     {currentPage > 1 && (
                         <button className="page-link"
@@ -112,9 +111,6 @@ export default function ListUser() {
                     )}
                 </div>
             </div>
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
         </>
     );
 }
